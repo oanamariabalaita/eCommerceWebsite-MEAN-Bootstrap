994bf16f50830f8bc1c1390dbf23cb068187e019
obalaita <oana4hi@gmail.com>
2018-12-13 22:22:08 +0200

    Added overlay image+responsive grid

--- src/app/ui/carousel/carousel.component.ts
+++ src/app/ui/carousel/carousel.component.ts
@@ -10,7 +10,7 @@ import { NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 export class CarouselComponent implements OnInit {
 
   constructor(config: NgbCarouselConfig)
-   { config.interval=1000;
+   { config.interval=2000;
      config.wrap = false;
      config.keyboard=false;
      config.pauseOnHover=false; }
